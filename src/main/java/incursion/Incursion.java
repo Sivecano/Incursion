03a764a8d393b50f380701b8e59b4151684fd0a2
Sivecano <Sivecano@gmail.com>
2022-08-18 22:00:23 +0200

    yes

--- src/main/java/incursion/Incursion.java
+++ src/main/java/incursion/Incursion.java
@@ -1,20 +1,23 @@
 package incursion;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Incursion implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final ItemGroup INCUSRSION_GROUP = FabricItemGroupBuilder.build(
+			new Identifier("tutorial", "general"),
+			() -> new ItemStack(Blocks.COBBLESTONE));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
 	}
