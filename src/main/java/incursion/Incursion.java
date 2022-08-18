43848e812e85c3364babfe3e6cb5c41a63095b9c
kaloka <kalokaprivat@gmail.com>
2022-08-18 23:26:34 +0200

    Add Nexus Block Entity with Textures
    Signed-off-by: kaloka <kalokaprivat@gmail.com>

--- src/main/java/incursion/Incursion.java
+++ src/main/java/incursion/Incursion.java
@@ -20,7 +20,7 @@ public class Incursion implements ModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("incursion");
     public static final Block BLOCK_NEXUS = new BlockNexus(FabricBlockSettings.of(Material.STONE).strength(100.0f));
     public static final BlockEntityType<BlockEntityNexus> BLOCK_ENTITY_NEXUS = Registry.register(
             Registry.BLOCK_ENTITY_TYPE,
